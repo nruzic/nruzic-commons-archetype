b074cd7292bc65258e62be93cf8608d2a260e804
Nikola Ruzic <nruzic.nixanet@gmail.com>
2018-08-29 11:31:05 +0200

    Addind ad archetype

---
+++ nruzic-commons-archetype-ad/src/main/resources/archetype-resources/src/test/java/AppTest.java
@@ -0,0 +1,34 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package};
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+class AppTest
+{
+    App app;
+
+    @BeforeEach
+    void setUp() throws Exception
+    {
+        // Pre test initialization code
+    }
+
+    @AfterEach
+    void tearDown() throws Exception
+    {
+        // Post test cleanup code
+    }
+
+    @Test
+    void testApp() throws Exception
+    {
+        assertNull(this.app);
+    }
+
+}
